544a31ec284857966170c6f08eff1f41e568e0fd
Jan Martinek <honza.martinek@gmail.com>
2021-12-15 12:14:59 +0100

    Site navigation

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -141,7 +141,61 @@ function setupTargetBlanks() {
   });
 }
 
+function setupActiveAriaRoles() {
+  document.querySelectorAll('._collapsible-nav .dropdown-menu').forEach(function (el) {
+    el.addEventListener('focusout', function (event) {
+      var dropdown = event.target.closest('.dropdown');
+
+      setTimeout(() => {
+        if (!dropdown.querySelector('.dropdown-menu').contains(document.activeElement)) {
+          dropdown.querySelector('label').setAttribute('aria-expanded', false);
+          dropdown.previousElementSibling.checked = false;
+        }
+      }, 100);
+    });
+  });
+
+  document.querySelectorAll('.opener').forEach(function (el) {
+    el.addEventListener('click', function (event) {
+      event.preventDefault();
+      if (event.pointerType === 'mouse' && event.target.classList.contains('keyboard-only-opener'))
+        return;
+
+      openCollapsible(event);
+    });
+
+    el.addEventListener('keyup', function (event) {
+      if (event.code == 'Enter') {
+        openCollapsible(event);
+      }
+    });
+  });
+}
+
+function openCollapsible(event) {
+  var id = event.target.getAttribute('for');
+  var input = document.getElementById(id);
+  var newState = !input.checked;
+
+  if (event.target.classList.contains('dropdown-title')) {
+    if (id !== 'help-checkbox') document.getElementById('help-checkbox').checked = false;
+    if (id !== 'work-checkbox') document.getElementById('work-checkbox').checked = false;
+  }
+
+  input.checked = newState;
+
+  if (event.target.classList.contains('nav-opener')) setNavOpeners(newState);
+  else event.target.setAttribute('aria-expanded', newState);
+}
+
+function setNavOpeners(state) {
+  document.querySelectorAll('.nav-opener').forEach(function (navOpener) {
+    navOpener.setAttribute('aria-expanded', state);
+  });
+}
+
 document.addEventListener('DOMContentLoaded', function () {
   initSearch();
   setupTargetBlanks();
+  setupActiveAriaRoles();
 });
