ca99b3fad7645bedde288c3b6773e69a6c513e57
Jan Martinek <honza.martinek@gmail.com>
2025-01-21 11:49:05 +0100

    Temporary: add backup old version of admin

---
+++ static-preview/admin-old/styles.js
@@ -0,0 +1,10 @@
+CKEDITOR.stylesSet.add('ochrance', [
+  // Block-level styles
+  { name: 'Odstavec', element: 'p' },
+  { name: 'Nadpis 2', element: 'h2' },
+  { name: 'Nadpis 3', element: 'h3' },
+  { name: 'Nadpis 4', element: 'h4' },
+  { name: 'Citace', element: 'blockquote' },
+
+  // Inline styles
+]);
